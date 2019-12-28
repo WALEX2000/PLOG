84a44d6e457d43850fbd8020d636ba9399086a97
Alex <up201705049@fe.up.pt>
2019-12-28 18:57:44 +0000

    estou-me a passar com isto

--- PROJ2/display.pl
+++ PROJ2/display.pl
@@ -33,15 +33,67 @@ printRoot(Total):-
     writeRepeat('     ', Total),
     print('|'), nl.
 
-%Prints a Row of the Tree
-printRow([Elem|Rest], TotalSpacing):-
+getRightMostChild([Elem|[]], Elem).
+getRightMostChild([Elem|Rest], RightMostChild):-
+    getRightMostChild(Rest, RightMostChild).
+
+getLeftMostWeight([], (1000000|_)). %Really big number to ensure success in case there are no weights left
+getLeftMostWeight([Elem|Rest], LeftMostWeight):-
+    isWeight(Elem), LeftMostWeight = Elem.
+
+getLeftMostWeight([_|Rest], LeftMostWeight):-
+    getLeftMostWeight(Rest).
+
+%Check if Subtree can begin there or needs to go down more
+checkIfSubtreeValid(Subtree, RightMostPos, Rest):-
+    Subtree = [LeftChild|OtherNodes],
+    LeftChild = (ChildPos|_), ChildPos >= RightMostPos,
+    %Now check if rightMostChild is in conflict with LeftMostWeight
+    getRightMostChild(OtherNodes, RightMostChild),
+    getLeftMostWeight(Rest, LeftMostWeight),
+    RightMostChild = (ChildPos|_),
+    LeftMostWeight = (WeightPos|_),
+    ChildPos < WeightPos, %The problem is that the ChildPos is relative to parent Tree and we need the global position here
+
+printRowItems([Elem|Rest], TotalSpacing, RightMostPos):-
+    Elem = (CurrPos|Node),
+    %If it's a weight then print('|111|')
+    ((isWeight(Node),
+      WS is TotalSpacing + CurrPos - 1,
+      writeRepeat('     ', WS), %Write white space until node is reached
+      ((number(Node), print('|'), print(Node), print('|')); %Print node if it has numbers on it TODO add whitespace to make up total of 3 spaces with numbers
+       (print('|???|'))), %Print node if it's unknown
+      printRowItems(Rest, CurrPos) %Go to next node TODO fix
+    );
+    %If it's a subTree then evaluate if it's possible to place it here
+        %Check if the leftmost Item overlaps with any weight or subtree on the left
+        %Check if the RightMost overlaps with any weight
+    (
+     %Check if it's possible to "unwrap" the tree here
+     checkIfSubtreeValid(Node, RightMostPos, Rest),
+     %if it is then calculate the amount of spacing to get there and start printing like in the printRow thing, then go to next item
+     %If it's not then calculate the spacing necessary and place a |. Then go to next item
+     WS is TotalSpacing + CurrPos,
+     writeRepeat('     ', WS),
+     print('|'),
+     printRowBottomRest(Rest, CurrPos))).
+    %if all good then write like on the Row
+    %If not then place a | instead
+    %Will have to return a list with all the nodes on the next level (New Nodes or leftovers)
+    
+isWeight(Node):- var(Node).
+isWeight(Node):- number(Node).
+
+printMargin(Margin):- writeRepeat('     ', Margin).
+
+printRow([Elem|Rest], RootGP):-
     Elem = (FirstPos|_),
-    WS is TotalSpacing + FirstPos,
+    WS is RootGP + FirstPos,
     writeRepeat('     ', WS),
     print('+'),
     printRowRest(Rest, FirstPos).
 
-printRowRest([], _):- nl.
+printRowRest([], _).
 printRowRest([Elem|Rest], LastPos):-
     Elem = (CurrPos|_),
     Spaces is CurrPos - LastPos - 1,
@@ -50,15 +102,15 @@ printRowRest([Elem|Rest], LastPos):-
     printRowRest(Rest, CurrPos).
 
 %Prints the bottom side of a Row
-printRowBottom([Elem|Rest], TotalSpacing):-
+printRowBottom([Elem|Rest], LastPos):-
     Elem = (CurrPos|Node),
     ((isWeight(Node),
-      WS is TotalSpacing + CurrPos - 1,
+      WS is LastPos + CurrPos - 1,
       writeRepeat('     ', WS),
       print(' _|_ '),
       printRowBottomRest(Rest, CurrPos)
      );
-     (WS is TotalSpacing + CurrPos,
+     (WS is LastPos + CurrPos,
      writeRepeat('     ', WS),
      print('|'),
      printRowBottomRest(Rest, CurrPos))).
@@ -76,18 +128,51 @@ printRowBottomRest([Elem|Rest], LastPos):-
       print('    |'),
       printRowBottomRest(Rest, CurrPos))).
 
-isWeight(Node):- var(Node).
-isWeight(Node):- number(Node).
+%Node can be a weightValue or a List
+printNode(WeightValue, WeightGP, CurrentPosition, ExtraSpace, WeightGP, 2):- %For Weights
+    isWeight(WeightValue),
+    printWeightSpacing, %Needs to check if there are any LeftOverSpaces and print the amount of spaces needed until this pos 
+    ((number(WeightValue), print('|'), print(WeightValue), print('|')); %TODO add whitespace to make up total of 3 spaces with numbers
+    (print('|???|'))), %Print node if it's unknown
+    Left.
+
+printNode(Subtree, RootGP, CurrentPosition, ExtraSpace, NewPos, 0):- %List (LIST might be impossible to print)
+    printRow(Subtree, RootGP),
+    getRightMostChild(Subtree, Child),
+    Child = (NewPos|_).
+
+printStem(Subtree, RootGP, CurrPos, ExtraSpace, NewPos, NewExtraSpace):-
+    printRowBottom(Subtree, RootGP),
+    getRightMostChild(Subtree, Child),
+    Child = (NewPos|_).
+
+%Prints a line of Subtrees and weights (Subtrees can be invalid and thus need to go down more)
+printLine([], _, _):- nl.
+printLine([Node/RootGP|Rest], CurrPos, ExtraSpace):-
+    printNode(Node, RootGP, CurrPos, ExtraSpace, NewPos, NewExtraSpace),
+    printLine(Rest, NewPos, NewExtraSpace).
+
+printLineBottom([], _, _):- nl.
+printLineBottom([Node/RootGP|Rest], CurrPos, ExtraSpace):-
+    printStem(Node, RootGP, CurrPos, ExtraSpace, NewPos, NewExtraSpace),
+    printLineBottom(Rest, NewPos, NewExtraSpace).
 
 %Main Function to print tree
 printTree:-
     complexTree(Tree),
-    Margin = 0,
+    Margin = 1,
     getLeftMostPosition(Tree, LeftMostValue),
-    MaxSpacing is abs(LeftMostValue),
-    Total is MaxSpacing + Margin,
-    printRoot(Total),
-    printRow(Tree, Total),
-    printRowBottom(Tree, Total).
+    RootGlobalPosition is abs(LeftMostValue),
+
+    printMargin(Margin),
+    printRoot(RootGlobalPosition),
+    printMargin(Margin),
+    printLine([Tree/RootGlobalPosition], 0, 0),
+    printMargin(Margin),
+    printLineBottom([Tree/RootGlobalPosition], 0, 0).
+
+   % printRow(Tree, RootGlobalPosition), %TODO Needs to change the 2nd argument here
+   % printMargin(Margin),
+   % printRowBottom(Tree, RootGlobalPosition). %TODO Needs to change the 2nd argument here
 
 % consult('display.pl'), printTree.
